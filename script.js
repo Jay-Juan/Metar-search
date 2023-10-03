5f3ab0b9d0fd05184b2acd869f050759c2c0ed6f
Juan Pablo <jpcorvo219@gmail.com>
2023-10-03 16:40:01 -0300

    Button fixed
    -METAR appears on button click

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 const API = 'https://avwx.rest/api/metar/';
 
 
-
-
 function traerMetar(funcion) {
     let icaoInput = document.getElementById("buscador")
     let icaoCode = icaoInput.value;
 
     fetch(`${API}/${icaoCode}`, {
-    method: "GET",
-    headers: {
-        "Authorization": "teTvrLx_-0c9XaWNWpr2qzJEia4gxlft5kRekDBp8Pg",
-    },
-})
-    .then(response => response.json())
-    .then(data => {
-        funcion(data)
-        console.log(API + icaoCode + ".json");
+        mode: "cors",
+        method: "GET",
+        headers: {
+            "Authorization": "teTvrLx_-0c9XaWNWpr2qzJEia4gxlft5kRekDBp8Pg",
+        }
     })
+        .then(response => response.json())
+        .then(data => {
+            funcion(data)
+            console.log(API + icaoCode + ".json");
+        })
 }
 
 function mostrarMetar(info) {
@@ -30,8 +29,11 @@ function mostrarMetar(info) {
     `
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    let botonRaw = document.getElementById("botonRaw")
 
-document.addEventListener("click", () => {
-    traerMetar(mostrarMetar);
-
+    
+    botonRaw.addEventListener("click", () => {
+        traerMetar(mostrarMetar)})
 })
+
