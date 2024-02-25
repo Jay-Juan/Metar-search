40775726be9752bd7f5ebcf3a4a355f007b64517
Juan Pablo <jpcorvo219@gmail.com>
2024-02-25 19:41:48 -0300

    structured METAR progress
    -icao code
    -time
    -wind readings
    -visibility
    -temperature/dewpoint
    -atmospheric pressure

--- script.js
+++ script.js
@@ -13,27 +13,42 @@ function traerMetar(funcion) {
         }
     })
         .then(response => response.json())
-        .then(data => {
-            funcion(data)
-            console.log(API + icaoCode + ".json");
-        })
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        .then(data => {funcion(data); console.log(API + icaoCode + ".json");});
+        // para ver el metar en consola
+        //.then(json => console.log(json))
+        ///////////////////////////////////////////////////////////////////////////////////////////
 }
 
 function mostrarMetar(info) {
     let contenedor = document.getElementById("container")
-
+    let icaoInput = document.getElementById("buscador")
+    let icaoCode = icaoInput.value;
     contenedor.innerHTML = `
     <div>
         <p>${info.raw}</p>
+        <br>
+        <p>${icaoCode.toUpperCase()}&nbsp${info.time.repr}&nbsp${info.wind_direction.repr}${info.wind_speed.repr}KT&nbsp${info.visibility.repr}&nbsp${info.temperature.repr}/${info.dewpoint.repr}&nbsp${info.altimeter.repr}</p>
     </div>
     `
+    //${info.}&nbsp
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     let botonRaw = document.getElementById("botonRaw")
 
-    
     botonRaw.addEventListener("click", () => {
-        traerMetar(mostrarMetar)})
+        traerMetar(mostrarMetar)
+    })
+
+    var input = document.getElementById("buscador");
+    input.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            botonRaw.click();
+            console.log()
+        }
+    });
 })
 
+// buscar los tipos de nubes que hayan disponibles y mostrarlos
